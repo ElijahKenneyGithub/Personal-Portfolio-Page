3fa83831b64f1eb79580a71aa4ca768a1102e5f1
elijahkenney <125106601+ElijahKenneyGithub@users.noreply.github.com>
2023-09-04 15:42:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document.querySelector(".home-image").addEventListener("mouseenter", function() {
-    var responses = [
-        "Yes, I'm this handsome in real life.",
-        "Future unclear, but my skills are sharp.",
-        "Very doubtful... that you've seen a portfolio this cool.",
-        "Without a doubt, you're in the right place."
-    ];
-    
-    var randomResponse = responses[Math.floor(Math.random() * responses.length)];
-    document.querySelector(".magic-response").textContent = randomResponse;
-});
+
